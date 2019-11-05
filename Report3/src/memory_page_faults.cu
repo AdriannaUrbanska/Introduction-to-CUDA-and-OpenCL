8ebb3d73985ec438a07edeb9ac039dd6ac2564d3
AdriannaUrbanska <ada.urbanska@wp.pl>
2019-11-05 23:49:40 +0100

    add profiling data

---
+++ Report3/src/memory_page_faults.cu
@@ -0,0 +1,48 @@
+_global__
+void deviceKernel(int *a, int N)
+{
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for (int i = idx; i < N; i += stride)
+  {
+    a[i] = 1;
+  }
+}
+
+void hostFunction(int *a, int N)
+{
+  for (int i = 0; i < N; ++i)
+  {
+    a[i] = 1;
+  }
+}
+
+int main()
+{
+
+  int N = 2<<24;
+  size_t size = N * sizeof(int);
+  int *a;
+  int threadsPerBlock = 256;
+  int blocksPerGrid = (N+threadsPerBlock-1)/threadsPerBlock;
+  cudaMallocManaged(&a, size);
+  hostFunction(a, N);  
+  deviceKernel<<<threadsPerBlock, blocksPerGrid>>>(a,N);
+  cudaDeviceSynchronize();
+  /*
+   * Conduct experiments to learn more about the behavior of
+   * `cudaMallocManaged`.
+   *
+   * What happens when unified memory is accessed only by the GPU?
+   * What happens when unified memory is accessed only by the CPU?
+   * What happens when unified memory is accessed first by the GPU then the CPU?
+   * What happens when unified memory is accessed first by the CPU then the GPU?
+   *
+   * Hypothesize about UM behavior, page faulting specificially, before each
+   * experiement, and then verify by running `nvprof`.
+   */
+
+  cudaFree(a);
+}
+
